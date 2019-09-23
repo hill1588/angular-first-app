d4fd61de90037349823eeeb22730ff22f481fd25
hill1588 <hill1588@morris.umn.edu>
2019-09-22 22:52:18 -0500

    update to app.components

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
